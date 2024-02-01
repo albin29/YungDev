4821c9ba0cd8dc8a9e696d1e39cf837dddd2ed4a
arkainas <kevin.vikander@gmail.com>
2024-02-01 10:46:13 +0100

    Added database connection to the database YungDev

--- Program.cs
+++ Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Npgsql;
 Console.WriteLine("Hello, World!");
+
+string dbUri = "Host=localhost;Port=5455;Username=postgres;Password=postgres;Database=YungDev";
+
+await using var db = NpgsqlDataSource.Create(dbUri);
+Console.Clear();
