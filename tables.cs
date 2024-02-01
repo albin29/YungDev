87bc1918e769269e5ba8c8a06f1f539f54cb205e
arkainas <kevin.vikander@gmail.com>
2024-02-01 13:33:15 +0100

    finished tables for player and playerstats

---
+++ tables.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Npgsql;
+
+
+string dbUri = "Host=localhost;Port=5455;Username=postgres;Password=postgres;Database=YungDev";
+
+await using var db = NpgsqlDataSource.Create(dbUri);
+
+/*
+await using (var cmd = db.CreateCommand("DROP TABLE IF EXISTS user"))
+{
+    await cmd.ExecuteNonQueryAsync();
+}
+
+await using (var cmd = db.CreateCommand("DROP TABLE IF EXISTS player_stats CASCADE"))
+{
+    await cmd.ExecuteNonQueryAsync();
+}
+
+*/
+
+string player = @"create table if not exists player(
+                id serial primary key,
+                name text,
+                password text
+)";
+
+await using (var cmd = db.CreateCommand(player))
+{
+    await cmd.ExecuteNonQueryAsync();
+}
+
+
+string playerStats = @"create table if not exists player_stats(
+                       player_id integer references player(id),
+                       programming_skill int,
+                       math_skill int,
+                       money int,
+                       stamina int,
+                       score int
+)";
+
+await using (var cmd = db.CreateCommand(playerStats))
+{
+    await cmd.ExecuteNonQueryAsync();
+}
+
